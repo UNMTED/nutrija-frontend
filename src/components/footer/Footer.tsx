5738a8f3d79d95c58f885b5f85ca7e7622cd4099
Jefferson Carvalho <jeffersoncarvalhodosreis@gmail.com>
2025-11-10 19:05:07 -0300

    build: atualiza dependencias

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,23 +1,46 @@
-import { FacebookLogoIcon,InstagramLogoIcon,LinkedinLogoIcon,} from "@phosphor-icons/react";
+import {
+    FacebookLogoIcon,
+    InstagramLogoIcon,
+    LinkedinLogoIcon,
+} from "@phosphor-icons/react";
 
 function Footer() {
     const data = new Date().getFullYear();
     return (
         <div className="w-full text-white flex justify-center py-4 border-t border-white">
             <div className="container flex flex-col items-center p-1">
-                <p className="text-sm font-bold">
-                    NutriJá | Copyright: {data}
-                </p>
-                <p className='text-lg'>Acesse nossas redes sociais</p>
-                <div className='flex gap-2'>
-                    <a href="https://www.linkedin.com/in/seu_usuario" target="_blank" rel="noopener noreferrer">
-                        <LinkedinLogoIcon size={46} weight='bold' />
+                <p className="text-sm font-bold">NutriJá | Copyright: {data}</p>
+                <p className="text-lg">Acesse nossas redes sociais</p>
+                <div className="flex gap-2">
+                    <a
+                        href="https://www.linkedin.com/in/seu_usuario"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <LinkedinLogoIcon
+                            size={46}
+                            weight="bold"
+                        />
                     </a>
-                    <a href="https://www.instagram.com/seu_usuario" target="_blank" rel="noopener noreferrer">
-                        <InstagramLogoIcon size={46} weight='bold' />
+                    <a
+                        href="https://www.instagram.com/seu_usuario"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <InstagramLogoIcon
+                            size={46}
+                            weight="bold"
+                        />
                     </a>
-                    <a href="https://www.facebook.com/seu_usuario" target="_blank" rel="noopener noreferrer">
-                        <FacebookLogoIcon size={46} weight='bold' />
+                    <a
+                        href="https://www.facebook.com/seu_usuario"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <FacebookLogoIcon
+                            size={46}
+                            weight="bold"
+                        />
                     </a>
                 </div>
                 <p className="text-xs">
@@ -28,4 +51,3 @@ function Footer() {
     );
 }
 export default Footer;
-
