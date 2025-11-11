169292a9ac46677f7b065ba9586ba661829c1335
MayaraOliveir <mayaraoliveir00@gmail.com>
2025-11-11 12:03:39 -0300

    feat(pages) adiciona login e componente FormAuth

--- src/App.tsx
+++ src/App.tsx
@@ -3,35 +3,52 @@ import { ToastContainer } from "react-toastify";
 import Footer from "./components/footer/Footer";
 import { AuthProvider } from "./contexts/AuthContext";
 import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
+
 
 import "react-toastify/dist/ReactToastify.css";
 import Navbar from "./components/navbar/Navbar";
 import Cadastro from "./pages/cadastro/Cadastro";
 import Inicio from "./pages/inicio/Inicio";
+import Login from "./pages/login/Login";
+
 
 function App() {
-  return (
-    <>
-      <AuthProvider>
-        <ToastContainer />
-        <BrowserRouter>
-          <Navbar />
-          <div className="max-w-7xl mx-auto p-2">
-            <div className="min-h-[80vh]">
-              <Routes>
-                <Route path="/" element={<Inicio />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/home" element={<Home />} />
-                <Route path="/cadastro" element={<Cadastro />} />
-              </Routes>
-            </div>
-          </div>
-          <Footer />
-        </BrowserRouter>
-      </AuthProvider>
-    </>
-  );
+
+    return (
+        <>
+            <AuthProvider>
+                <ToastContainer />
+                <BrowserRouter>
+                    <Navbar />
+                    <div className="max-w-7xl mx-auto p-2">
+                        <div className="min-h-[80vh]">
+                            <Routes>
+                            
+                                   <Route
+                                    path="/"
+                                    element={<Inicio />}
+                                />
+                                <Route
+                                    path="/login"
+                                    element={<Login />}
+                                />
+                                <Route
+                                    path="/home"
+                                    element={<Home />}
+                                />
+                                <Route
+                                    path="/cadastro"
+                                    element={<Cadastro />}
+                                />
+                            </Routes>
+                        </div>
+                    </div>
+                    <Footer />
+                </BrowserRouter>
+            </AuthProvider>
+        </>
+    );
+
 }
 
 export default App;
